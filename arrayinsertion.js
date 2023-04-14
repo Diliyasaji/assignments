2d9371da78c8bb884811350a4ea96ae09cb8e931
diliya saji <diliyasindhusaji@gmail.com>
2023-04-14 10:15:53 +0530

    arrayinsertion commit

---
+++ arrayinsertion.js
@@ -0,0 +1,9 @@
+var array = [1,2,3,4,5,6,7];
+console.log(array);
+var index = prompt(`enter the required index: `);
+var element = prompt(`enter the no.to be changed: `);
+for (i = array.length; i > index; i--) {
+array[i] = array[i - 1];
+}
+array[index] = element;
+console.log(`the changed array is:`,array);
