3e6bb5358054aedd4598cd904f51723c1be41ed2
diliya saji <diliyasindhusaji@gmail.com>
2023-04-14 09:56:59 +0530

    array commit

---
+++ array.js
@@ -0,0 +1,9 @@
+<script>
+var numbers = [1,2,3,4,5,6,7];
+var even = [];
+console.log(`the array is: `,numbers);
+for(let i = 0; i < numbers.length; i++) {
+       if (numbers[i] % 2 == 0)
+       even.push(numbers[i]);
+}
+console.log(`Even numbers in an array are: ${even}`);
