950be5b2c61303d8aa5f1752fc283ba62b97bf68
Diliya Saji <115158520+Diliyasaji@users.noreply.github.com>
2023-04-18 19:22:17 +0530

    evenarray.js

--- array.js
+++ array.js
@@ -1,9 +1,9 @@
 // js program to find even no.from an array
 var numbers = [1,2,3,4,5,6,7];
-var even = [];
+var even = [];//array to store even nos.
 console.log(`the array is: `,numbers);
 for(let i = 0; i < numbers.length; i++) {
        if (numbers[i] % 2 == 0)
-       even.push(numbers[i]);
+       even.push(numbers[i]);//elements inserted to the end of array even
 }
 console.log(`Even numbers in an array are: ${even}`);
